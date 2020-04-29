717f7651abc31c419b81cbf072c3c15a782fb1a9
jpccc <1778013127@qq.com>
2020-04-30 01:17:17 +0800

    初步实现语法分析，所需在readme

--- Compiler/Compiler.cpp
+++ Compiler/Compiler.cpp
@@ -1,32 +1,39 @@
 ﻿// Compiler.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
-
 #include <iostream>
 #include<fstream>
 #include "LexicalAnalyzerByJPC.h"
 #include"LexicalAnalyzerByTS.h"
+#include"GrammarSource.h"
 using namespace std;
 void ReadChar(string fileName, string& input);
 int main()
 {
     /********************************读入源程序********************************/
-    string filename = "test/test0.txt";//debug目录下
+    string filename = "file/test1.txt";//debug目录下
     string input;//存储源程序
     ReadChar(filename, input);
     /***********************词法分析返回Token序列,注释直接丢弃****************/
     LexicalAnalyzerByJPC lexicalAnalyzer(input);
     lexicalAnalyzer.makeTokenList();
     Token* t = lexicalAnalyzer.getToken();
+    Token* start = t;
+    
     cout << "Token序列如下" << endl;
-    while (t != NULL) {
+    while (t!= NULL) {
         cout << t->value << "  " << t->type << endl;
         t = t->next;
     }
     
     /********************************语法分析********************************/
 
-    //LexicalAnalyzerByTS::debugging();
-    
+    GrammarAnalyzer grammarAnalyzer(start);
+    grammarAnalyzer.initialize();
+    //grammarAnalyzer.test();
+    if(grammarAnalyzer.GrammarAnalyzers())
+        cout << "语法分析成功" << endl;
+    else
+        cout << "语法分析失败" << endl;
 }
 void ReadChar(string fileName,string& input) {
     fstream inFile;
@@ -45,5 +52,6 @@ void ReadChar(string fileName,string& input) {
             inFile >> c;
         }
     }
+    inFile.close();
     input.append(1, EOF);
 }
